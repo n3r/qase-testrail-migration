7fed071efda2fd40f76d28554d75a4f0afe38ea8
Nikita Fedorov <nik333r@gmail.com>
2023-12-04 13:58:41 +0300

    Config_manager update

--- src/support/config_manager.py
+++ src/support/config_manager.py
@@ -23,20 +23,56 @@ class ConfigManager:
     def _get_keys(self, config, prefix=""):
         for key, value in config.items():
             if isinstance(value, dict):
-                yield from self._get_keys(value, f"{prefix}{key}_")
+                yield from self._get_keys(value, f"{prefix}{key}.")
             else:
                 yield f"{prefix}{key}"
 
     def _set_config(self, key, value):
-        keys = key.split("_")
+        keys = key.split(".")
         config = self.config
         for key in keys[:-1]:
             config = config.setdefault(key, {})
         config[keys[-1]] = value
 
     def _get_config(self, key):
-        keys = key.split("_")
+        keys = key.split(".")
         config = self.config
         for key in keys[:-1]:
             config = config.get(key, {})
         return config.get(keys[-1], None)
+    
+    def build_config(self):
+        config = {}
+
+        print("Please enter the following configuration details. Press Enter for default value.")
+
+        # Qase Configuration
+        config['qase']['token'] = input("Enter Qase token: ")
+        config['qase']['host'] = input("Enter Qase host (default: api.qase.io): ") or "api.qase.io"
+
+        # Testrail Configuration
+        connection_type = input("Enter Testrail connection (api/db): ")
+        if connection_type.lower() == "api":
+            config['testrail']['connection'] = "api"
+            config['testrail']['api']['password'] = input("Enter Testrail API password: ")
+            config['testrail']['api']['host'] = input("Enter Testrail API host: ")
+            config['testrail']['api']['user'] = input("Enter Testrail API user: ")
+        elif connection_type.lower() == "db":
+            config['testrail']['connection'] = "db"
+            config['testrail']['db']['DB_HOST'] = input("Enter Testrail DB host: ")
+            config['testrail']['db']['DB_USER'] = input("Enter Testrail DB user: ")
+            config['testrail']['db']['DB_PASSWORD'] = input("Enter Testrail DB password: ")
+            config['testrail']['db']['DB_NAME'] = input("Enter Testrail DB name: ")
+            config['testrail']['db']['DB_PORT'] = input("Enter Testrail DB port: ")
+
+        # Projects Configuration
+        projects_import = input("Enter project names for import, separated by commas (default: []): ") or ""
+        config['projects']['import'] = [project.strip() for project in projects_import.split(',')] if projects_import else []
+        completed_input = input("Are the projects completed? (True/False, default: True): ")
+        config['projects']['completed'] = True if completed_input.lower() in ['true', ''] else False
+
+        # Save the configuration to a JSON file
+        with open('config.json', 'w') as config_file:
+            json.dump(config, config_file, indent=4)
+
+        print("Configuration saved to config.json")
